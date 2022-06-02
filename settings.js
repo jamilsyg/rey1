0108d5fca7bffb96e32d53c74d0eb6d05ae7e7ee
jamilsyg <106760587+jamilsyg@users.noreply.github.com>
2022-06-02 23:11:03 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,22 +27,22 @@ global.premium = ['6289675134806'] //ubah jadi nomor mu, note tanda ' gausah di 
 global.ownernomer = '6289675134806' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'REY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Riyadh' //ubah jadi nama mu, note tanda ' gausah di hapus!
 global.botname = 'Emilia - MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'REY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.footer = 'Riyadh' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
 global.youtube = '-'
 global.ig = 'https://instagram.com/bangreyy_54'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = '-'
+global.mygc = 'https://chat.whatsapp.com/Fs0c8M54Kj2CLEZx185yci'
+global.myweb = 'https://chat.whatsapp.com/Fs0c8M54Kj2CLEZx185yci'
 global.email = 'bundasarimbit@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
 global.packname = '© Emilia - MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'REY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.author = 'Rey' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
